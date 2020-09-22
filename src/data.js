79054646596ac869c844d733ca37ec60c7c5c5e3
Paulo Ilarde <pauloilarde@gmail.com>
2020-09-21 21:43:20 -0300

    Adicionando dados dos produtos

---
+++ src/data.js
@@ -0,0 +1,274 @@
+export default {
+    products: [
+        {
+            "featured": true,
+            "id": "1",
+            "sku": "sku-1",
+            "path": "camiseta-preta",
+            "name": "Camiseta Preta",
+            "image": "/assets/media/shirt-1.jpg",
+            "price": 59.9,
+            "filter": [
+              {
+                "color": "Preta"
+              }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "2",
+            "sku": "sku-2",
+            "path": "camiseta-laranja",
+            "name": "Camiseta Laranja",
+            "image": "/assets/media/shirt-2.jpg",
+            "price": 40.5,
+            "filter": [
+                {
+                    "color": "Laranja"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "3",
+            "sku": "sku-3",
+            "path": "camiseta-amarela",
+            "name": "Camiseta Amarela",
+            "image": "/assets/media/shirt-3.jpg",
+            "price": 61.49,
+            "filter": [
+                {
+                    "color": "Amarela"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "4",
+            "sku": "sku-4",
+            "path": "camiseta-rosa",
+            "name": "Camiseta Rosa",
+            "image": "/assets/media/shirt-4.jpg",
+            "price": 52.0,
+            "filter": [
+                {
+                    "color": "Rosa"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "10",
+            "sku": "sku-10",
+            "path": "calca-social",
+            "name": "Calça Social",
+            "image": "/assets/media/pants-1.jpg",
+            "price": 39.9,
+            "specialPrice": 29.9,
+            "filter": [
+                {
+                "gender": "Masculina"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "11",
+            "sku": "sku-11",
+            "path": "calca-feminina",
+            "name": "Calça Feminina",
+            "image": "/assets/media/pants-2.jpg",
+            "price": 24.15,
+            "filter": [
+                {
+                    "gender": "Feminina"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "12",
+            "sku": "sku-12",
+            "path": "calca-la-feminina",
+            "name": "Calça de Lã Feminina",
+            "image": "/assets/media/pants-3.jpg",
+            "price": 61.49,
+            "filter": [
+                {
+                    "gender": "Feminina"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "13",
+            "sku": "sku-13",
+            "path": "calca-caminhada-masculina",
+            "name": "Calça Caminhada Masculina",
+            "image": "/assets/media/pants-4.jpg",
+            "price": 70.0,
+            "specialPrice": 58,
+            "filter": [
+                {
+                "gender": "Masculina"
+                }
+            ]
+        },
+        {
+            "featured": true,
+            "id": "14",
+            "sku": "sku-14",
+            "path": "calca-caminhada-feminina",
+            "name": "Calça Caminhada Feminina",
+            "image": "/assets/media/pants-5.jpg",
+            "price": 70.0,
+            "filter": [
+                {
+                "gender": "Feminina"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "31",
+            "sku": "sku-31",
+            "path": "tenis-preto-couro",
+            "name": "Tênis Preto Couro",
+            "image": "/assets/media/shoes-1.jpg",
+            "price": 129.9,
+            "filter": [
+                {
+                "color": "Preta"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "32",
+            "sku": "sku-32",
+            "path": "tenis-preto",
+            "name": "Tênis Preto Masculino",
+            "image": "/assets/media/shoes-2.jpg",
+            "price": 140.5,
+            "filter": [
+                {
+                    "color": "Laranja"
+                }
+            ]
+        },
+        {
+            "featured": true,
+            "id": "33",
+            "sku": "sku-33",
+            "path": "tenis-corrida",
+            "name": "Tênis Corrida",
+            "image": "/assets/media/shoes-3.jpg",
+            "price": 261.49,
+            "filter": [
+                {
+                    "color": "Amarela"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "34",
+            "sku": "sku-34",
+            "path": "tenis-corrida-gel",
+            "name": "Tênis Corrida Gel",
+            "image": "/assets/media/shoes-4.jpg",
+            "price": 352.0,
+            "filter": [
+                {
+                    "color": "Cinza"
+                }
+            ]
+        },
+        {
+            "featured": true,
+            "id": "35",
+            "sku": "sku-35",
+            "path": "tenis-slip",
+            "name": "Tênis Slip Azul",
+            "image": "/assets/media/shoes-5.jpg",
+            "price": 92.0,
+            "filter": [
+                {
+                    "color": "Azul"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "36",
+            "sku": "sku-36",
+            "path": "tenis-slip-preto",
+            "name": "Tênis Slip Preto",
+            "image": "/assets/media/shoes-6.jpg",
+            "price": 90.0,
+            "specialPrice": 50.9,
+            "filter": [
+                {
+                    "color": "Preto"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "37",
+            "sku": "sku-37",
+            "path": "tenis-corrida-rosa",
+            "name": "Tênis Corrida - Rosa ",
+            "image": "/assets/media/shoes-7.jpg",
+            "price": 315.0,
+            "filter": [
+                {
+                    "color": "Rosa"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "38",
+            "sku": "sku-38",
+            "path": "tenis-rosa",
+            "name": "Tênis Rosa Feminino",
+            "image": "/assets/media/shoes-8.jpg",
+            "price": 281.0,
+            "filter": [
+                {
+                    "color": "Rosa"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "39",
+            "sku": "sku-39",
+            "path": "tenis-casual",
+            "name": "Tênis Casual Bege",
+            "image": "/assets/media/shoes-9.jpg",
+            "price": 198.0,
+            "specialPrice": 180.9,
+            "filter": [
+                {
+                    "color": "Bege"
+                }
+            ]
+        },
+        {
+            "featured": false,
+            "id": "40",
+            "sku": "sku-40",
+            "path": "tenis-corrida-feminino-azul",
+            "name": "Tênis Corrida Feminino Azul",
+            "image": "/assets/media/shoes-10.jpg",
+            "price": 329.0,
+            "filter": [
+                {
+                    "color": "Azul"
+                }
+            ]
+        }
+    ]
+}
